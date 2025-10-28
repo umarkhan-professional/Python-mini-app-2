7c4fa9a2d4266266ab849fa96ce4087a8eb24d15
umar1 <umarkhan.professional@gmail.com>
2025-10-28 09:11:15 +0530

    eight

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask - CI test successful!......."
+    return "Hello from Flask - CI test successful!."
 
 if __name__ == "__main__":
     app.run()
